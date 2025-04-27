d5fd4f54a9a8565f3d56732b0067dc51cf6e61b8
xinyu <yu.life@qq.com>
2025-04-27 17:44:30 +0800

    mysql漂移

--- server2/keepalived1/init-keepalived.sh
+++ server2/keepalived1/init-keepalived.sh
@@ -13,16 +13,16 @@ echo "Using interface: $INTERFACE"
 # 生成 keepalived 配置文件
 cat > /etc/keepalived/keepalived.conf << EOF
 global_defs {
-    script_security
+   enable_script_security
+   script_user root
 }
 
-# 检查 MySQL 健康的脚本
 vrrp_script chk_mysql {
     script "/etc/keepalived/check_mysql.sh"
     interval 2
-    weight -10       
-    fall 2          
-    rise 1 
+    weight -50
+    fall 1
+    rise 1
 }
 
 vrrp_instance VI_WEB {
@@ -53,14 +53,16 @@ vrrp_instance VI_DB {
         auth_pass 123456
     }
     virtual_ipaddress {
-        10.0.0.20/8
+        172.16.0.100/16
     }
     track_script {
         chk_mysql
     }
-    notify_master "/bin/sh -c 'ipvsadm -A -t 10.0.0.20:3306 -s rr && ipvsadm -a -t 10.0.0.20:3306 -r 172.16.0.21:3306 -m && ipvsadm -a -t 10.0.0.20:3306 -r 172.16.0.22:3306 -m'"
-    notify_backup "/bin/sh -c 'ipvsadm -D -t 10.0.0.20:3306'"
-    notify_fault "/bin/sh -c 'ipvsadm -D -t 10.0.0.20:3306'"
+    notify_master "/bin/sh -c 'ipvsadm -A -t 172.16.0.100:3306 -s rr && \
+        ipvsadm -a -t 172.16.0.100:3306 -r 172.16.0.21:3306 -m && \
+        ipvsadm -a -t 172.16.0.100:3306 -r 172.16.0.22:3306 -m'"
+    notify_backup "/bin/sh -c 'ipvsadm -D -t 172.16.0.100:3306'"
+    notify_fault "/bin/sh -c 'ipvsadm -D -t 172.16.0.100:3306'"
 }
 EOF
 
